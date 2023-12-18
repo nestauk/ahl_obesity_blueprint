5bf7bfe350764a2b5d682dce9ad9d586fd2fe90e
Anish Chacko <you@example.com>
2023-12-18 19:00:00 +0000

    updated code with outputs

--- models/adult_model.R
+++ models/adult_model.R
@@ -1,5 +1,10 @@
-rm(list = ls())
-#install.packages("svydesign")
+# Policy 4a :  Ban BOGOF price promotions on HFSS products for medium and large retailers
+# Effect size from rapid reviews = -22 kcal reduction
+# Intervention group: Adults with BMI >= 25 [overweight, obese and morbidly obese]
+# Outputs: Year wise BMI bar plot and corresponding table
+
+#rm(list = ls())
+install.packages("svydesign")
 install.packages("ggplot2")
 
 library(tidyverse)
@@ -7,43 +12,48 @@ library(here)
 library(bw)
 library(survey)
 
-# explore the vignettes
-#browseVignettes("bw")
 
-# policy 1: tv online advertising watershed = 0 kcals
-# policy 2: volume price promotions = 22 kcals
-# policy 3: nutri-score fopl = 50 kcals
-
-
+# variable for change in intake as a result of the policy
 intake_change = -22
 
-df_adult <- read_csv(here("inputs/processed/hse_2019.csv")) %>%  # we are reading a file that contains energy intake, BMI weight class and other variables reated during the pre-processing stage.
-  #head(20) %>% 
+# Reading the processed HSE 2019 data that contains energy intake, BMI weight class and other variables
+# created during the pre-processing stage.
+# The intake change is applied only to those who are overweight, obese and morbidly obese
+df_adult <- read_csv(here("inputs/processed/hse_2019.csv")) %>%  
   mutate(sex = ifelse(sex == 1, "male", "female")) %>%
-  #mutate(intake_diff = -intake*0.01) %>%
   mutate(intake_diff = ifelse(bmi_class %in% c("underweight", "normal"), 0, intake_change))
 
-
+# For each individual/ observation in the HSE, we apply the difference in energy intake[intake_diff],
+# (converting it into type numeric), then creating a vector, which essentially repeats the intake diff
+# value 365*5 times (i.e. for a five year period), which is then transposed to match the input requirements
+# of the Hall Calorie Weight Model.
 eichange <- t(apply(df_adult, 1, function(x) rep(as.numeric(x["intake_diff"]), 365*5)))
-# for each individual in the hse, we are taking the difference in energy intake[intake_diff], (converting it into type numeric),
-# then creating a vector, which essentially repeats the intake diff value 365 times (i.e. for a year)
-# this is then done for each individual (row in the df) and then transposed.
-# this matrix of intake difference for a year is input to create the weight model.
 
-#, NAchange = rep(0, 365*3)
+# A matrix of change in salt consumption set to zero is another input to the model. This is set to zero as
+# information on change in salt consumption is not available from our rapid reviews
 nachange <- t(apply(df_adult, 1, function(x) rep(0, 365*5)))
 
-model_weight <- adult_weight(df_adult$weight, df_adult$height/100, df_adult$age, df_adult$sex, eichange, nachange, days = 365*5)
 # the bw package has a function called [adult_weight] that takes the following inputs:
-# weight of the individual, height f the individual in meters, age, sex and change in 
-# energy intake (for a period of time)
+# baseline body weight, height (in meters), age, sex and change in energy intake (for a five year period)
+model_weight <- adult_weight(df_adult$weight, df_adult$height/100, df_adult$age, df_adult$sex, eichange, nachange, days = 365*5)
 
+model_weight <- adult_weight(bw = df_adult$weight,
+                             ht = df_adult$height/100,
+                             age = df_adult$age,
+                             sex = df_adult$sex,
+                             EIchange = eichange,
+                             NAchange = nachange,
+                             days = 365*5)
 
+# Extracting BMI values from the model and joining them to the HSE dataset for further analysis 
+# and output generation. 'bmi_model' is a matrix of day wise change in BMI of the population as a result of
+# the intervention. 
 bmi_model = model_weight[["Body_Mass_Index"]]
-
 new_df = cbind(df_adult, bmi_model)
 
 
+# Creating a new dataframe with variables of interest and BMI values at the end of each of the five years
+# of the intervention. Subsequently, categorising observations into BMI categories for each year.
 post_df_adult = new_df %>%
   select("id", "weight", "height", "age", "sex", "bmi", "wt_int", "psu", 
          "strata", "pal", "rmr", "bmi_class", "intake", "intake_diff", "1", "365", "730", "1095", "1460", "1825" ) %>%
@@ -85,7 +95,8 @@ post_df_adult = new_df %>%
                                  bmi_5 >= 40 ~ "morbidly obese",
                                  TRUE ~ "NA"))
 
-
+# survey design specification to estimate population level effects of the intervention. Survey design spec is
+# created using weightings from HSE 2019.
 design <-  svydesign(ids=~post_df_adult$psu, 
                      nest = T,
                      data=post_df_adult,
@@ -93,38 +104,7 @@ design <-  svydesign(ids=~post_df_adult$psu,
 
 
 
-# density plot
-
-density_plot = ggplot(post_df_adult, aes(x= bmi, fill = "bmi_start")) +
-  geom_density(alpha = 0.5) +
-  geom_density(data = post_df_adult, aes(x = bmi_5, fill = "bmi_end", alpha = 0.5)) +
-  labs(title = "bmi_start and bmi_end", x = "BMI", y = "density") +
-  scale_fill_manual(values=c("#0000FF", "#F6A4B7"))
-
-density_plot
-
-
-## clustered bar plot
-
-post_df_adult_long = post_df_adult %>%
-  select(c(bmi_class, bmi_5_class)) %>%
-  gather(key = "variable", value = "category")
-
-percentage_df <- post_df_adult_long %>%
-  group_by(variable, category) %>%
-  summarise(freq = n()) %>%
-  mutate(percentage = freq / sum(freq) * 100)
-
-bar_plot <- ggplot(data = percentage_df, aes(x = category, y = percentage, fill = variable)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "BMI Categories Comparison", x = "BMI Category", y = "Percentage")
-#  scale_fill_manual(values = c("#0000FF", "#F6A4B7"))
-
-bar_plot
-
-
-
-
+# creating a table of year wise distribution of BMI categories
 bmi_change = rbind(
   post_df_adult %>% 
     count(bmi_5_class, wt = wt_int) %>% 
@@ -157,28 +137,12 @@ bmi_change = rbind(
            type = "Year 0") %>% 
     rename(BMI = bmi_class))
 
-
 bmi_change = bmi_change %>%
-  mutate(BMI = factor(BMI, levels = c("underweight", "normal", "overweight", "obese", "morbidly obese")))
+  mutate(BMI = factor(BMI, levels = c("underweight", "normal", "overweight", "obese", "morbidly obese"))) %>%
+  as.data.frame()
 
-bmi_change = as.data.frame(bmi_change)
-
-bmi_change_year = bmi_change %>%
-  select(-c(n)) %>%
-  pivot_wider(., names_from = BMI, values_from = freq)
- 
-
-
-bmi_change_year_1 = bmi_change %>%
-  select(-c(freq)) %>%
-  pivot_wider(., names_from = BMI, values_from = n)
-
-
-write_csv(bmi_change_year_1, here("outputs/data/bmi_change_year_p2_1.csv"))
-
-
-
-bmi_change %>%
+# Output 1: Plot of year on year BMI category distribution
+adult_bar_plot = bmi_change %>%
   ggplot(., aes(y = freq, x = BMI, fill = type)) + 
   geom_bar(stat = "identity", position = "dodge") +
   theme_ipsum() +
@@ -186,16 +150,15 @@ bmi_change %>%
        title = "BMI Categories Distribution", 
        y = "Frequency",
        subtitle = "Population") +
-  theme_ipsum(base_size = 8, axis_title_size = 8, base_family="Averta") +
-  #scale_fill_manual(values=c("#0000FF", "#F6A4B7")) +
+  theme_ipsum(base_size = 8, axis_title_size = 8) + #, base_family="Averta"
   theme(legend.position = "top")
 
+ggsave(here("outputs/figures/adult_bmi_distrib_policy4a.png"), plot = adult_bar_plot, width = 10, height = 6, bg='#ffffff')
 
+# Output 2: Table of year wise prevalence of obesity
+bmi_change_year = bmi_change %>%
+  select(-c(n)) %>%
+  pivot_wider(., names_from = BMI, values_from = freq)
 
-
-
-
-
-
-
+write_csv(bmi_change_year, here("outputs/data/adult_bmi_distrib_policy4a.csv"))
 
--- models/child_model.R
+++ models/child_model.R
@@ -1,5 +1,11 @@
-rm(list = ls())
-install.packages("survey")
+# Policy 4a :  Ban BOGOF price promotions on HFSS products for medium and large retailers
+# Effect size from rapid reviews = -22 kcal reduction
+# Intervention group: Children identified as overweight, obese or severely obese
+# Outputs: BMI bar plot comparison between baseline and endline years and corresponding table
+
+
+#rm(list = ls())
+install.packages("svydesign")
 install.packages("ggplot2")
 
 library(tidyverse)
@@ -7,14 +13,14 @@ library(here)
 library(bw)
 library(survey)
 
-browseVignettes("bw")
 
+child_intake_change = -5
 
 df_child <- read_csv(here("inputs/processed/hse_2019_children.csv")) %>%  # we are reading a file that contains energy intake, BMI weight class and other variables reated during the pre-processing stage.
-  #head(20) %>% 
   mutate(sex = ifelse(sex == 1, "male", "female")) %>% 
   filter(fm_hudda > 0) %>%
-  mutate(intake_diff = -intake*0.01)
+  mutate(intake_diff = ifelse(bmi_cat_baseline %in% c("underweight", "normal"), 0, child_intake_change ))
+
 
 
 
@@ -22,7 +28,7 @@ df_child <- read_csv(here("inputs/processed/hse_2019_children.csv")) %>%  # we a
 
 #t_eichange_child <- t(apply(df_child, 1, function(x) rep(as.numeric(x["intake"]) + as.numeric(x["intake_diff"]), 365*1)))
 
-nt_ei = apply(df_child, 1, function(x) rep(as.numeric(x["intake"]) + as.numeric(x["intake_diff"]), 365*5))
+energy_intake = apply(df_child, 1, function(x) rep(as.numeric(x["intake_hox"]) + as.numeric(x["intake_diff"]), 365*5))
 
 
 # model 1 with fm and ffm
@@ -30,20 +36,9 @@ child_model_weight = child_weight(age = df_child$age,
                                   sex =  df_child$sex, 
                                   FM = df_child$fm_hudda, 
                                   FFM = df_child$ffm_hudda, 
-                                  EI = nt_ei,   
+                                  EI = energy_intake,   
                                   days = (365*5),
                                   checkValues = TRUE)
-
-# model 2 without inputting fm and ffm
-# child_model_weight_1 = child_weight(age = df_child$age,
-#                                     sex =  df_child$sex, 
-#                                     #FM = df_child$fm_hudda, 
-#                                     #FFM = df_child$ffm_hudda, 
-#                                     EI = nt_ei,   
-#                                     days = (365*5),
-#                                     checkValues = TRUE)
-# 
-
 
 
 child_bw_h = as.data.frame(child_model_weight[["Body_Weight"]]) %>%
@@ -79,79 +74,97 @@ child_fm_h = as.data.frame(child_model_weight[["Fat_Mass"]]) %>%
 
 child_age_bw_ffm_fm = cbind(child_bw_h, child_age_h, child_ffm_h, child_fm_h) %>%
   select(age_1, bw_1, fm_1, ffm_1,
-         age_365, bw_365, fm_365, ffm_365,
-         age_730, bw_730, fm_730, ffm_730,
-         age_1095, bw_1095, fm_1095, ffm_1095,
-         age_1460, bw_1460, fm_1460, ffm_1460,
+         # age_365, bw_365, fm_365, ffm_365,
+         # age_730, bw_730, fm_730, ffm_730,
+         # age_1095, bw_1095, fm_1095, ffm_1095,
+         # age_1460, bw_1460, fm_1460, ffm_1460,
          age_1825, bw_1825, fm_1825, ffm_1825)
 
 
 
 
-# Deurenbery et al. (1991)
-
-child_bw_end_1 = as.data.frame(child_model_weight_1[["Body_Weight"]])
-
-
-child_bw_end_check_d = child_bw_end_1 %>%
-  mutate(bw_diff = V1825 - V1) %>%
-  select(c(V1, V365, V730, V1095, V1460, V1825, bw_diff))
-
-
-
-sum(child_bw_end_check$V1 == 19.5)
-
-new_df_child = cbind(df_child, child_bw_end_check)
-
-
-
-df_new_child = 
-
-model_plot(child_model_weight, c("Fat_Mass"))
-
-model_plot(child_model_weight_1, c("Body_Weight"))
-
-
-
-
-
-## Test:
-
-mydata <- data.frame(
-  id = 1:5,
-  age = c(8, 10, 7, 7, 12),
-  sex = c("male", "female", "female", "male", "male"),
-  energy = runif(5, 1500, 2000),
-  prob = c(0.1, 0.2, 0.2, 0.05, 0.45))
-
-#Get energy change with energy build function
-eichange <- energy_build(cbind(runif(5, 1500, 2000), mydata$energy), c(0, 365))
-
-t_eichange = t(eichange)
-
-#Returns a weight change matrix and other matrices
-database_model <- child_weight(mydata$age, mydata$sex, EI = t(eichange))
-
-cbind(runif(5, 1500, 2000), mydata$energy)
-
-model_plot(database_model, "Body_Weight")
-
-energy_build()
+new_df_child = cbind(df_child, child_age_bw_ffm_fm) %>%
+  mutate(bf_percent_end = ((fm_1825/ bw_1825)*100)) %>%
+  mutate(bmi_cat_endline = case_when(age == 6 ~ (case_when(sex == "male" ~ case_when(bf_percent_end <= 12.4 ~ "underweight",
+                                                                                  bf_percent_end >= 19.5 & bf_percent_end < 22.7 ~ "overweight",
+                                                                                  bf_percent_end >12.4 & bf_percent_end < 19.5 ~ "normal",
+                                                                                  bf_percent_end >= 22.7 ~ "obese"),
+                                                        sex == "female" ~ case_when(bf_percent_end <= 14.4 ~ "underweight",
+                                                                                    bf_percent_end >14.4 & bf_percent_end < 23 ~ "normal",
+                                                                                    bf_percent_end >= 23 & bf_percent_end < 26.2 ~ "overweight",
+                                                                                    bf_percent_end >= 26.2 ~ "obese") )),
+                                  age == 9 ~ (case_when(sex == "male" ~ case_when(bf_percent_end <= 12.8 ~ "underweight",
+                                                                             bf_percent_end >12.8 & bf_percent_end < 22.2 ~ "normal",
+                                                                             bf_percent_end >= 22.2 & bf_percent_end < 26.8 ~ "overweight",
+                                                                             bf_percent_end >= 26.8 ~ "obese"),
+                                                        sex == "female" ~ case_when(bf_percent_end <= 15.7 ~ "underweight",
+                                                                             bf_percent_end >15.7 & bf_percent_end < 27.2 ~ "normal",
+                                                                             bf_percent_end >= 27.2 & bf_percent_end < 31.2 ~ "overweight",
+                                                                             bf_percent_end >= 31.2 ~ "obese") )),
+                                  age == 11.5 ~ (case_when(sex == "male" ~ case_when(bf_percent_end <= 12.6 ~ "underweight",
+                                                                                bf_percent_end >12.6 & bf_percent_end < 23 ~ "normal",
+                                                                                bf_percent_end >= 23 & bf_percent_end < 28.3 ~ "overweight",
+                                                                                bf_percent_end >= 28.3 ~ "obese"),
+                                                           sex == "female" ~ case_when(bf_percent_end <= 16.1 ~ "underweight",
+                                                                                bf_percent_end >16.1 & bf_percent_end < 28.8 ~ "normal",
+                                                                                bf_percent_end >= 28.8 & bf_percent_end < 32.8 ~ "overweight",
+                                                                                bf_percent_end >= 32.8 ~ "obese") )),
+                                  age == 14 ~ (case_when(sex == "male" ~ case_when(bf_percent_end <= 10.9 ~ "underweight",
+                                                                              bf_percent_end >10.9 & bf_percent_end < 21.3 ~ "normal",
+                                                                              bf_percent_end >= 21.3 & bf_percent_end < 25.9 ~ "overweight",
+                                                                              bf_percent_end >= 25.9 ~ "obese"),
+                                                         sex == "female" ~ case_when(bf_percent_end <= 16 ~ "underweight",
+                                                                              bf_percent_end > 16 & bf_percent_end < 29.6 ~ "normal",
+                                                                              bf_percent_end >= 29.6 & bf_percent_end < 33.6 ~ "overweight",
+                                                                              bf_percent_end >= 33.6 ~ "obese") )),
+                                  age == 17 ~ (case_when(sex == "male" ~ case_when(bf_percent_end <= 9.8 ~ "underweight",
+                                                                                   bf_percent_end > 9.8 & bf_percent_end < 20.1 ~ "normal",
+                                                                                   bf_percent_end >= 20.1 & bf_percent_end < 23.9 ~ "overweight",
+                                                                                   bf_percent_end >= 23.9 ~ "obese"),
+                                                         sex == "female" ~ case_when(bf_percent_end <= 15.1 ~ "underweight",
+                                                                                   bf_percent_end > 15.1 & bf_percent_end < 30.4 ~ "normal",
+                                                                                   bf_percent_end >= 30.4 & bf_percent_end < 34.4 ~ "overweight",
+                                                                                   bf_percent_end >= 34.4 ~ "obese") )) ))
 
 
 
+child_bmi_change = rbind(
+  new_df_child %>% 
+    count(bmi_cat_endline, wt = wt_int) %>% 
+    mutate(freq = n/sum(n)*100,
+           type = "Year 5") %>% 
+    rename(BMI = bmi_cat_endline),
+  new_df_child %>% 
+    count(bmi_cat_baseline, wt = wt_int) %>% 
+    mutate(freq = n/sum(n)*100,
+           type = "Year 0") %>% 
+    rename(BMI = bmi_cat_baseline))
+
+child_bmi_change = child_bmi_change %>%
+  mutate(BMI = factor(BMI, levels = c("underweight", "normal", "overweight", "obese", "morbidly obese"))) %>%
+  as.data.frame()
 
 
 
+# Output 1: Plot of year on year BMI category distribution
+child_bar_plot = child_bmi_change %>%
+  ggplot(., aes(y = freq, x = BMI, fill = type)) + 
+  geom_bar(stat = "identity", position = "dodge") +
+  theme_ipsum() +
+  labs(fill = "", 
+       title = "BMI Categories Distribution", 
+       y = "Frequency",
+       subtitle = "Population") +
+  theme_ipsum(base_size = 8, axis_title_size = 8) + #, base_family="Averta"
+  theme(legend.position = "top")
 
+child_bar_plot
 
+ggsave(here("outputs/figures/child_bmi_distrib_policy4a.png"), plot = child_bar_plot, width = 10, height = 6, bg='#ffffff')
 
+# Output 2: Table of year wise prevalence of obesity
+child_bmi_change_year = child_bmi_change %>%
+  select(-c(n)) %>%
+  pivot_wider(., names_from = BMI, values_from = freq)
 
-
-
-
-
-
-
-
-
+write_csv(child_bmi_change_year, here("outputs/data/child_bmi_distrib_policy4a.csv"))
